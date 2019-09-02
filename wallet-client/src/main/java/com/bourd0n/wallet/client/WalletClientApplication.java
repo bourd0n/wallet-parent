cfa1c2f353b28df68aa917309dc726a5ab5b5e01
bourd0n <andone74@gmail.com>
2019-09-02 17:48:02 +0300

    Start implementing CLI

--- wallet-client/src/main/java/com/bourd0n/wallet/client/WalletClientApplication.java
+++ wallet-client/src/main/java/com/bourd0n/wallet/client/WalletClientApplication.java
@@ -3,10 +3,80 @@ package com.bourd0n.wallet.client;
 import com.bourd0n.wallet.api.grpc.*;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import org.apache.commons.cli.*;
 
 public class WalletClientApplication {
+
+    private static final Options OPTIONS = new Options();
+
+    private static final Option WALLET_SERVER_HOST = Option.builder("h")
+            .longOpt("walletServerHost")
+            .hasArg()
+            .desc("Wallet server host. By default: localhost")
+            .required(false)
+            .build();
+
+    private static final Option WALLET_SERVER_PORT = Option.builder("p")
+            .longOpt("walletServerPort")
+            .type(Integer.class)
+            .hasArg()
+            .desc("Wallet server port. By default: 8081")
+            .required(false)
+            .build();
+
+    private static final Option NUMBER_OF_USERS = Option.builder("u")
+            .longOpt("usersCount")
+            .type(Integer.class)
+            .hasArg()
+            .desc("Number of concurrent users emulated")
+            .required()
+            .build();
+
+    private static final Option NUMBER_OF_THREADS = Option.builder("t")
+            .longOpt("threadCount")
+            .type(Integer.class)
+            .hasArg()
+            .desc("Number of concurrent requests a user will make")
+            .required()
+            .build();
+
+    private static final Option ROUNDS_PER_THREAD = Option.builder("r")
+            .longOpt("roundsPerThread")
+            .type(Integer.class)
+            .hasArg()
+            .desc("Number of rounds each thread is executing")
+            .required()
+            .build();
+
+    static {
+        OPTIONS.addOption(WALLET_SERVER_HOST);
+        OPTIONS.addOption(WALLET_SERVER_PORT);
+        OPTIONS.addOption(NUMBER_OF_USERS);
+        OPTIONS.addOption(NUMBER_OF_THREADS);
+        OPTIONS.addOption(ROUNDS_PER_THREAD);
+    }
+
     public static void main(String[] args) {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8081)
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(OPTIONS, args);
+        } catch (ParseException e) {
+            System.err.println(e);
+            formatter.printHelp("wallet-client", OPTIONS);
+            System.exit(1);
+        }
+
+        //todo: typesafety
+        String walletServerHost = cmd.getOptionValue(WALLET_SERVER_HOST.getOpt(), "localhost");
+        Integer walletServerPort = Integer.valueOf(cmd.getOptionValue(WALLET_SERVER_PORT.getOpt(), "8081"));
+        Integer numberOfUsers = Integer.valueOf(cmd.getOptionValue(NUMBER_OF_USERS.getOpt()));
+        Integer numberOfThreads = Integer.valueOf(cmd.getOptionValue(NUMBER_OF_THREADS.getOpt()));
+        Integer roundsPerThread = Integer.valueOf(cmd.getOptionValue(ROUNDS_PER_THREAD.getOpt()));
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(walletServerHost, walletServerPort)
                 .usePlaintext()
                 .build();
 
